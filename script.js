e009ea83c1858844264cc087ea5194a7a173614d
sajid210177 <161884984+sajid210177@users.noreply.github.com>
2024-12-31 02:04:19 +0600

    commit message

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 // Script for navigation bar
+const bar = document.getElementById('bar');
+const nav = document.getElementById('nabvar');
+
+if (bar) {
+    bar.addEventListener('click', () =>{
+        nav.classList.add('active');
+    })
+}
